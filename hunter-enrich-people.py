126d200ea1883ac62f75fea8e6e84d85ee9f8dee
Aaron Williams <aaron@flex.io>
2020-04-28 15:03:43 -0500

    added fault-tolerant request mechanism

--- hunter-enrich-people.py
+++ hunter-enrich-people.py
@@ -52,8 +52,10 @@
 # ---
 
 import json
-import requests
 import urllib
+import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 import itertools
 from datetime import *
 from cerberus import Validator
@@ -125,7 +127,7 @@ def flexio_handler(flex):
         url = 'https://api.hunter.io/v2/email-finder?' + url_query_str
 
         # get the response data as a JSON object
-        response = requests.get(url)
+        response = requests_retry_session().get(url)
         response.raise_for_status()
         content = response.json()
         content = content.get('data', {})
@@ -141,6 +143,25 @@ def flexio_handler(flex):
     except:
         flex.output.content_type = 'application/json'
         flex.output.write([['']])
+
+def requests_retry_session(
+    retries=3,
+    backoff_factor=0.3,
+    status_forcelist=(500, 502, 504),
+    session=None,
+):
+    session = session or requests.Session()
+    retry = Retry(
+        total=retries,
+        read=retries,
+        connect=retries,
+        backoff_factor=backoff_factor,
+        status_forcelist=status_forcelist,
+    )
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    return session
 
 def validator_list(field, value, error):
     if isinstance(value, str):
--- hunter-verify-email.py
+++ hunter-verify-email.py
@@ -54,9 +54,11 @@
 # ---
 
 import json
-import requests
-import itertools
 import urllib
+import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+import itertools
 from datetime import *
 from cerberus import Validator
 from collections import OrderedDict
@@ -126,7 +128,7 @@ def flexio_handler(flex):
         url = 'https://api.hunter.io/v2/email-verifier?' + url_query_str
 
         # get the response data as a JSON object
-        response = requests.get(url)
+        response = requests_retry_session().get(url)
         response.raise_for_status()
         content = response.json()
         content = content.get('data', {})
@@ -142,6 +144,25 @@ def flexio_handler(flex):
     except:
         flex.output.content_type = 'application/json'
         flex.output.write([['']])
+
+def requests_retry_session(
+    retries=3,
+    backoff_factor=0.3,
+    status_forcelist=(500, 502, 504),
+    session=None,
+):
+    session = session or requests.Session()
+    retry = Retry(
+        total=retries,
+        read=retries,
+        connect=retries,
+        backoff_factor=backoff_factor,
+        status_forcelist=status_forcelist,
+    )
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    return session
 
 def debug_properties_map():
     properties_iter = map(lambda prop : prop + ' => ' + properties_map.get(prop, ''), properties_map)
